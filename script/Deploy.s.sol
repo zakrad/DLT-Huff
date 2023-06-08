96dfd7bade2fab7dbeff8692e0dcb1895553bbb3
zakrad <daniyal.rad@gmail.com>
2023-06-08 15:14:45 +0330

    ✅ modify deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,13 +4,28 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-interface SimpleStore {
-  function setValue(uint256) external;
-  function getValue() external returns (uint256);
+interface DLT {
+    function mint(address, uint256, uint256, uint256, bytes calldata) external;
+    function burn(address, uint256, uint256, uint256) external;
+
+    function isApprovedForAll(address, address) external view returns (bool);
+    function setApprovalForAll(address, bool) external;
+
+    function safeTransferFrom(address, address, uint256, uint256, uint256, bytes calldata) external;
+
+    function subBalanceOf(address, uint256, uint256) external view returns (uint256);
+
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
 }
 
 contract Deploy is Script {
-  function run() public returns (SimpleStore simpleStore) {
-    simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
-  }
+
+    DLT public dlt;
+
+    function run() public returns (DLT dlt) {
+        string memory wrappers = vm.readFile("test/mock/DLTWrappers.huff");
+        HuffDeployer.config().with_args(bytes.concat(abi.encode("DualLayerToken"), abi.encode("DLT")));
+        dlt = DLT(HuffDeployer.deploy_with_code("DLT", wrappers));
+    }
 }
